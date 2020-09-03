50a775ab73702614056d996f68c2f010cff88958
vukolovanton <vukolovanton92@gmail.com>
2020-09-03 21:17:38 +0400

    Add connection to DB

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+const mongoose = require('mongoose');
+
 // READING ENVOIRMENT
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
-// RUNNING APP
 const app = require('./app.js');
 
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connection to DB');
+  });
+
+// RUNNING APP
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('App is running');
+  console.log('App is running');
 });
