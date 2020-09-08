a9e594f04daf63f309b37485fbc1a1db7ad9bcda
vukolovanton <vukolovanton92@gmail.com>
2020-09-08 16:08:13 +0400

    Defining user and auth schemas

---
+++ model/userModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A tour must have a email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'A tour must have a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  // If password wasn't modifietd just return
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,20 @@
 const express = require('express');
+
 const router = express.Router();
 
-const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require('../controllers/userController');
+const { signup } = require('../controllers/authController');
+const {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userController');
 
-// ROUTES 
-router.route('/')
-    .get(getAllUsers)
-    .post(createUser);
-router.route('/:id')
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser); 
+router.post('/signup', signup);
+
+// ROUTES
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
